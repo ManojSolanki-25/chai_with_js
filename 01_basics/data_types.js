cd9f63d166148ca60178f0a325ea9c68a12115b5
Manoj Solanki <99466266+ManojSolanki-25@users.noreply.github.com>
2025-05-18 11:46:39 +0000

    data types

--- 01_basics/data_types.js
+++ 01_basics/data_types.js
@@ -1,4 +1,6 @@
-//  PRIMITIVE DATATYES
+//  PRIMITIVE DATATYPES
+
+//  7 types of primitive datatypes => Number, String, BigInt, Boolean, Null, Undefined, Symnbol
 
 let name = "manoj"
 let age = 12
@@ -13,3 +15,39 @@ console.log(typeof isMarried) // boolean
 console.log(typeof null) // object and also null is standalone value
 console.log(typeof undefined) // undefined
 console.table([name, age, isMarried])  // print a table 
+
+// symbol use for uniq
+const id = Symbol(101)
+const ontherId = Symbol(101)
+
+console.log(id === ontherId); // false
+
+// null is special type of data type it is standalone value and typeof is object
+console.log(typeof null)
+
+
+// reference (non primitive data type)
+
+
+// Array, Object and function
+// all noo primitive data types are typeof object 
+// function is typeof function object but it also count into object
+
+
+let names = ['manoj', 'yagyam', 'yogesh', 'shubham']
+let namesObject = {
+    name: "manoj",
+    age: 101
+}
+
+
+
+const myFunction = function(){
+    console.log("Hello World");
+};
+
+
+console.log(typeof names); // object
+console.log(typeof namesObject); //object
+console.log(typeof myFunction); // function and also called function object
+
